eea2d7fabee423bf7ffab8c39e5de74d24b6c074
mateusz <mateusz.okarmus@gmail.com>
2016-11-21 19:38:41 +0100

    Create project for auth-service and dockerize it

---
+++ auth-service/src/main/java/org/okarmus/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package org.okarmus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+}
